a86bb007c72d580b4cb2e84ecaf366ce4558bbaa
Noah H. Smith <noah@batteredoats.com>
2014-11-19 17:32:31 -0800

    use retry_join instead of start_join

--- recipes/_service.rb
+++ recipes/_service.rb
@@ -75,15 +75,15 @@ when 'cluster'
   service_config['server'] = true
   if num_cluster > 1
     service_config['bootstrap_expect'] = num_cluster
-    service_config['start_join'] = node['consul']['servers']
+    service_config['retry_join'] = node['consul']['servers']
   else
     service_config['bootstrap'] = true
   end
 when 'server'
   service_config['server'] = true
-  service_config['start_join'] = node['consul']['servers']
+  service_config['retry_join'] = node['consul']['servers']
 when 'client'
-  service_config['start_join'] = node['consul']['servers']
+  service_config['retry_join'] = node['consul']['servers']
 else
   Chef::Application.fatal! %Q(node['consul']['service_mode'] must be "bootstrap", "cluster", "server", or "client")
 end
